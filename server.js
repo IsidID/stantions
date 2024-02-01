1ef3fa867f517f43d6fc195edfcfc4d97fbb511d
romanbilokinsky <isidzukuro@gmail.com>
2024-02-01 15:53:33 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fetch = require('node-fetch');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+
+  fetch('https://net.tnt-tpi.com/page')
+    .then((response) => response.text())
+    .then((htmlString) => {
+      const regex = /<script type="text\/javascript">(.*?)<\/script>/s;
+      const match = htmlString.match(regex);
+      let content;
+
+      if (match) {
+        const scriptCode = match[1].trim();
+        eval(scriptCode); // get bslist var here
+        const stations = bslist.filter((item) => ['CHTK', 'CHR2', 'GRD2', 'CHEM'].includes(item[0]));
+        content = `<ul>${stations.map((item) => `<li>${item[3]}</li><li>Назва станції - ${item[0]}</li><li>Доступність - ${item[5]}</li>`)
+          .join('</ul><ul>')}</ul>`;
+      } else {
+        content = 'no data';
+      }
+      res.end(`<!DOCTYPE html>
+                  <html>
+                    <head>
+                      <meta charset="utf-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1">
+                      <title>Strange things</title>
+                    </head>
+                    <body><button onclick="location.reload();">Refresh</button>${content}</body>
+                  </html>
+      `);
+    });
+});
+
+// Specify the port on which the server will listen
+const port = 3000;
+
+// Start the server and listen on the specified port
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
