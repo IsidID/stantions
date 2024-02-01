e632e2c54abd5f7dae2cf5af89a16a7ec5bf25b6
Roman Bilokinskyi <114356321+IsidID@users.noreply.github.com>
2024-02-01 17:21:50 +0200

    Update server2.js

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const http = require('http');
 const fetch = require('node-fetch');
 const fs = require('fs/promises');
 
-if (!process.env.NETLIFY) {
-  // Only run the server if not in Netlify build environment
-  const generateHTML = async () => {
+const generateHTML = async () => {
+  try {
     const htmlString = await fetch('https://net.tnt-tpi.com/page').then((response) => response.text());
     const regex = /<script type="text\/javascript">(.*?)<\/script>/s;
     const match = htmlString.match(regex);
@@ -36,9 +35,28 @@ if (!process.env.NETLIFY) {
 
     await fs.writeFile('dist/index.html', htmlContent, 'utf-8');
     console.log('index.html generated successfully.');
-  };
+  } catch (error) {
+    console.error('Error generating index.html:', error);
+  }
+};
 
-  generateHTML();
+if (!process.env.NETLIFY) {
+  // Only run the server if not in Netlify build environment
+  const server = http.createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+
+    // Generate index.html before responding to requests
+    await generateHTML();
+
+    // Read and send the generated index.html
+    const indexContent = await fs.readFile('dist/index.html', 'utf-8');
+    res.end(indexContent);
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
+  });
 } else {
   console.log('Running in Netlify build environment. Server will not be started.');
 }
