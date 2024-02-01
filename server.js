16e919501c92fab7866df7f5a7e1fb7cf80c4b53
Roman Bilokinskyi <114356321+IsidID@users.noreply.github.com>
2024-02-01 17:04:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,47 @@
 const http = require('http');
+const fs = require('fs');
 const fetch = require('node-fetch');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 
-  // Fetch data from external source
-  fetch('https://net.tnt-tpi.com/page')
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Failed to fetch data, status: ${response.status}`);
-      }
-      return response.text();
-    })
-    .then((htmlString) => {
-      // Parse HTML and extract necessary data
-      const regex = /<script type="text\/javascript">(.*?)<\/script>/s;
-      const match = htmlString.match(regex);
-      let content;
+  const indexPath = './dist/index.html';
 
-      if (match) {
-        const scriptCode = match[1].trim();
-        eval(scriptCode); // get bslist var here
+  fs.readFile(indexPath, 'utf8', (err, htmlString) => {
+    if (err) {
+      console.error(`Error reading ${indexPath}:`, err);
+      res.end('Error reading index.html');
+      return;
+    }
 
-        // Filter stations based on specific criteria
-        const stations = bslist.filter((item) => ['CHTK', 'CHR2', 'GRD2', 'CHEM'].includes(item[0]));
+    const regex = /<script type="text\/javascript">(.*?)<\/script>/s;
+    const match = htmlString.match(regex);
+    let content;
 
-        // Generate HTML content
-        content = `<ul>${stations.map((item) => `<li>${item[3]}</li><li>Назва станції - ${item[0]}</li><li>Доступність - ${item[5]}</li>`)
-          .join('</ul><ul>')}</ul>`;
-      } else {
-        content = 'no data';
-      }
+    if (match) {
+      const scriptCode = match[1].trim();
+      eval(scriptCode); // get bslist var here
+      const stations = bslist.filter((item) => ['CHTK', 'CHR2', 'GRD2', 'CHEM'].includes(item[0]));
+      content = `<ul>${stations
+        .map(
+          (item) =>
+            `<li>${item[3]}</li><li>Назва станції - ${item[0]}</li><li>Доступність - ${item[5]}</li>`
+        )
+        .join('</ul><ul>')}</ul>`;
+    } else {
+      content = 'no data';
+    }
 
-      // Send HTML response to the client
-      res.end(`<!DOCTYPE html>
-                  <html>
-                    <head>
-                      <meta charset="utf-8">
-                      <meta name="viewport" content="width=device-width, initial-scale=1">
-                      <title>Strange things</title>
-                    </head>
-                    <body><button onclick="location.reload();">Refresh</button>${content}</body>
-                  </html>
-      `);
-    })
-    .catch((error) => {
-      // Handle errors during data fetching
-      console.error('Error fetching data:', error);
-      res.end('Error fetching data');
-    });
+    res.end(`<!DOCTYPE html>
+                <html>
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Strange things</title>
+                  </head>
+                  <body><button onclick="location.reload();">Refresh</button>${content}</body>
+                </html>`);
+  });
 });
 
 // Use the process.env.PORT or 3000 if not defined
