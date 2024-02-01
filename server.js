436814e753e363ff53ec082d14c94d1cc33c3ddc
Roman Bilokinskyi <114356321+IsidID@users.noreply.github.com>
2024-02-01 16:56:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 const http = require('http');
 const fetch = require('node-fetch');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+if (!process.env.NETLIFY) {
+  // Only run the server if not in Netlify build environment
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 
-  fetch('https://net.tnt-tpi.com/page')
-    .then((response) => response.text())
-    .then((htmlString) => {
-      const regex = /<script type="text\/javascript">(.*?)<\/script>/s;
-      const match = htmlString.match(regex);
-      let content;
+    fetch('https://net.tnt-tpi.com/page')
+      .then((response) => response.text())
+      .then((htmlString) => {
+        const regex = /<script type="text\/javascript">(.*?)<\/script>/s;
+        const match = htmlString.match(regex);
+        let content;
 
-      if (match) {
-        const scriptCode = match[1].trim();
-        eval(scriptCode); // get bslist var here
-        const stations = bslist.filter((item) => ['CHTK', 'CHR2', 'GRD2', 'CHEM'].includes(item[0]));
-        content = `<ul>${stations.map((item) => `<li>${item[3]}</li><li>Назва станції - ${item[0]}</li><li>Доступність - ${item[5]}</li>`)
-          .join('</ul><ul>')}</ul>`;
-      } else {
-        content = 'no data';
-      }
-      res.end(`<!DOCTYPE html>
-                  <html>
-                    <head>
-                      <meta charset="utf-8">
-                      <meta name="viewport" content="width=device-width, initial-scale=1">
-                      <title>Strange things</title>
-                    </head>
-                    <body><button onclick="location.reload();">Refresh</button>${content}</body>
-                  </html>
-      `);
-    });
-});
+        if (match) {
+          const scriptCode = match[1].trim();
+          eval(scriptCode); // get bslist var here
+          const stations = bslist.filter((item) => ['CHTK', 'CHR2', 'GRD2', 'CHEM'].includes(item[0]));
+          content = `<ul>${stations.map((item) => `<li>${item[3]}</li><li>Назва станції - ${item[0]}</li><li>Доступність - ${item[5]}</li>`)
+            .join('</ul><ul>')}</ul>`;
+        } else {
+          content = 'no data';
+        }
+        res.end(`<!DOCTYPE html>
+                    <html>
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <title>Strange things</title>
+                      </head>
+                      <body><button onclick="location.reload();">Refresh</button>${content}</body>
+                    </html>
+        `);
+      });
+  });
 
-// Use the process.env.PORT or 3000 if not defined
-const port = process.env.PORT || 3000;
-
-// Start the server and listen on the specified port
-server.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}/`);
-});
+  const port = process.env.PORT || 3000;
+  server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}/`);
+  });
+}
